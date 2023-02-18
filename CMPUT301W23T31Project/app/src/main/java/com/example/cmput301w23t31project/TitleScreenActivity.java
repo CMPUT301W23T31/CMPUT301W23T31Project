b37cbd74a88bf576ed0d26e9b4511f2ae0dc4b32
Josh Kammermayer <kammerma@ualberta.ca>
2023-02-18 15:12:05 -0700

    connect title to home

---
+++ CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/TitleScreenActivity.java
@@ -0,0 +1,21 @@
+package com.example.cmput301w23t31project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_title_screen);
+    }
+    public void onTap(View view){
+        Intent intent = new Intent(this, HomeScreenActivity.class);
+        startActivity(intent);
+    }
+}
