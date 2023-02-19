0e8179213f4024f021f1135adb03add0e6726928
Lyndon Hallett <lhallett@ualberta.ca>
2023-02-19 16:07:07 -0700

    Refined Utilities Class

--- CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/Utilities.java
+++ CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/Utilities.java
@@ -1,53 +1,73 @@
 package com.example.cmput301w23t31project;
+// The Utilities class contains methods needed throughout the entire program that
+// need are used to assist in more important tasks
 
-import android.content.Context;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-
-/**
- * The Utilities class contains methods needed throughout the entire program that
- * need are used to assist in more important tasks
- */
 // References
 // 1. https://www.geeksforgeeks.org/how-to-read-qr-code-using-zxing-library-in-android/
 // 2. https://www.baeldung.com/sha-256-hashing-java
 // 3. https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class Utilities {
 
+    /**
+     * This function obtains a score for a scanned QR Code
+     * @param hash
+     *      The hash value of a scanned QR code
+     * @return
+     *      The score assigned to a particular QR Code
+     */
     public static int getQRScore(String hash) {
         int stringLength = hash.length();
         int score = 0;
-        for (int i = 0; i < stringLength - 2;) {
+        for (int i = 0; i < stringLength - 2; i++) {
             int number = 1;
             while (hash.charAt(i) == hash.charAt(i+1)) {
                 i++;
                 number += 1;
             }
             if (number > 1) {
-                score += Math.pow(Integer.parseInt(String.valueOf(hash.charAt(i-1)),
-                        16), number-1);
-            } else {
-                i++;
+                if (hash.charAt(i-1) == '0') {
+                    score += Math.pow(20, number - 1);
+                } else {
+                    score += Math.pow(Integer.parseInt(String.
+                            valueOf(hash.charAt(i - 1)), 16), number - 1);
+                    i--;
+                }
             }
         }
         return score;
     }
 
+    /**
+     * This function obtains a SHA-256 hash for provided QR Code contents
+     * @param contents
+     *      The contents of the QR Code as a String of characters
+     * @return
+     *      The SHA-256 hash for a QR Code
+     * @throws NoSuchAlgorithmException
+     *      Throws such an exception when a "SHA-256" algorithm is
+     *      not found in getInstance
+     */
     public static String hashQRCode(String contents) throws NoSuchAlgorithmException {
         final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        final byte[] hashbytes = digest.digest(
+        final byte[] hashBytes = digest.digest(
                 contents.getBytes(StandardCharsets.UTF_8));
-        return bytesToHex(hashbytes);
+        return bytesToHex(hashBytes);
     }
 
+    /**
+     * This function converts bytes to their corresponding hex value
+     * @param hash
+     *      The hash for a particular QR Code
+     * @return
+     *      A string of hexadecimal values representing a QR Code
+     */
     private static String bytesToHex(byte[] hash) {
         StringBuilder hexString = new StringBuilder(2 * hash.length);
         for (int i = 0; i < hash.length; i++) {
@@ -59,32 +79,4 @@ public class Utilities {
         }
         return hexString.toString();
     }
-
-    public static String getQRCodeName(String hash, Context context) throws IOException {
-        String[] words = {"dog", "Cat", "pup"};
-        String name = "";
-        for (int i = 0; i < hash.length(); i += 8) {
-            int tempScore = 0;
-            for (int j = 0; j < i + 8; j++) {
-                tempScore += hash.charAt(j);
-            }
-            tempScore %= words.length;
-            name = name + words[tempScore];
-        }
-        return name;
-    }
-
-    public static String[] ReadFile(Context context) throws IOException {
-        BufferedReader br
-                = new BufferedReader(new InputStreamReader(context.getAssets().open("filename.txt")));
-        String st;
-        String[] contents = new String[100];
-        int count = 0;
-        while ((st = br.readLine()) != null) {
-            contents[count] = st;
-            count++;
-        }
-        return contents;
-    }
-
 }
