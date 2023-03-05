9e72a6bd8917311487737671ca37726f5e6f3056
Josh Kammermayer <kammerma@ualberta.ca>
2023-03-05 16:01:05 -0700

    qrcodestats database link attempt1

---
+++ CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/PlayerScansDataBase.java
@@ -0,0 +1,46 @@
+package com.example.cmput301w23t31project;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class PlayerScansDataBase {
+    FirebaseFirestore playerDB;
+    CollectionReference collection;
+    QueryDocumentSnapshot returnDocument = null;
+    QuerySnapshot[] collectionDocuments = new QuerySnapshot[1];
+
+    public PlayerScansDataBase(String collection) {
+        playerDB = FirebaseFirestore.getInstance();
+        this.collection = playerDB.collection(collection);
+    }
+
+    public QueryDocumentSnapshot getDocument(String docId) {
+        collection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document: task.getResult()) {
+                        if (document.getId().equals(docId)) {
+                            returnDocument = document;
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+        return returnDocument;
+    }
+
+    public CollectionReference getReference() {
+        return collection;
+    }
+
+
+}
+
