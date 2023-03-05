4924ddce546a9dbc818e61e51e689479d4692911
Josh Kammermayer <kammerma@ualberta.ca>
2023-03-05 13:51:16 -0700

    player/qrstuff

--- CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/ScanResultsFragment.java
+++ CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/ScanResultsFragment.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
