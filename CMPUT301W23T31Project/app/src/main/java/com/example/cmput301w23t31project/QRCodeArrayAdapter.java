7197723c7909345225881fdb8f506b021f19e58f
saumyarada <saumya.rada@gmail.com>
2023-03-07 18:19:21 -0700

    displaying all the qr codes for player (MyScans)

--- CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/QRCodeArrayAdapter.java
+++ CMPUT301W23T31Project/app/src/main/java/com/example/cmput301w23t31project/QRCodeArrayAdapter.java
@@ -1,10 +1,15 @@
 package com.example.cmput301w23t31project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -31,18 +36,19 @@ public class QRCodeArrayAdapter extends ArrayAdapter<QRCode> {
         View view = convertView;
 
         if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.content, parent, false);
+            view = LayoutInflater.from(context).inflate(R.layout.content_code_detail, parent, false);
         }
         else {
             view = convertView;
         }
 
         QRCode qrCode = getItem(position);
-        TextView QRCodeName = view.findViewById(R.id.QR_code_name);
-        TextView QRCodePoints = view.findViewById(R.id.QR_code_points);
+        TextView QRCodeName = view.findViewById(R.id.code_detail_name);
+        TextView QRCodePoints = view.findViewById(R.id.code_detail_points);
 
+        Log.d(TAG,"ADAPT:"+ qrCode.getName());
         QRCodeName.setText(qrCode.getName());
-        QRCodePoints.setText(qrCode.getScore());
+        QRCodePoints.setText("Points: "+Integer. toString(qrCode.getScore()));
 
         return view;
     }
